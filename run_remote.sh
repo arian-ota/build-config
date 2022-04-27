2277335ecbc2529967a12d544cd11b161096e92c
ArianK16a <arian.kulmer@web.de>
2022-04-27 21:29:20 +0100

    Add wakeonlan

--- run_remote.sh
+++ run_remote.sh
@@ -5,5 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if [[ ! -f /home/arian/.remote_mac.txt ]]; then
+    return 1
+fi
+
+wakeonlan $(cat ~/.remote_mac.txt)
+sleep 120
 ssh arian@home.local 'bash -s' < build.sh
 
